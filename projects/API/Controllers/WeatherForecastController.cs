d4e650812747f4e6ec22cb2db0c4289dc751bccd
peanut-lover <jihoson94@gmail.com>
2023-06-04 23:59:25 +0900

    Change to solution based project

--- projects/API/Controllers/WeatherForecastController.cs
+++ projects/API/Controllers/WeatherForecastController.cs
@@ -1,6 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
+using Microsoft.AspNetCore.Mvc;
 
-namespace SampleTodoApp.Controllers;
+namespace API.Controllers;
 
 [ApiController]
 [Route("[controller]")]
@@ -23,7 +24,7 @@ public class WeatherForecastController : ControllerBase
     {
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Date = DateTime.Now.AddDays(index),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
